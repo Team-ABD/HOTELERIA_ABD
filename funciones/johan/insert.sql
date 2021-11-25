7147b22ff6b1e8eb166bc12f2b72a48de93bc71d
Johan <johan.04120108@gmail.com>
2021-11-25 07:29:35 -0500

    main

---
+++ funciones/johan/insert.sql
@@ -0,0 +1,22 @@
+INSERT INTO tipo_comprobante
+	(tipo_comprobante_id, descripcion)
+VALUES
+	('01','Factura'),
+	('03','Boleta');
+	
+
+INSERT INTO tipo_Persona
+	(tipo_persona_id, descripcion)
+VALUES
+	(1,'Persona Natural'),
+	(2,'Persona Jurídica');
+
+INSERT INTO tipo_documento
+	(tipo_documento_id, descripcion)
+VALUES
+	('01','DNI'),
+	('04','Carnet de Extrangería'),
+	('06','RUC'),
+	('07','Pasaporte'),
+	('11','Partida de Nacimiento'),
+	('00','Otros');
