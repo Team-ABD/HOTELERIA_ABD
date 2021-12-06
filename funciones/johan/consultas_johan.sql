d41868703c179934d0dd318d9cc258f34b8c3530
Johan <johan.04120108@gmail.com>
2021-12-05 19:21:41 -0500

    main

--- funciones/johan/consultas_johan.sql
+++ funciones/johan/consultas_johan.sql
@@ -1,5 +1,9 @@
 -- Los mejores 5 clientes de cada tipo (natural y jurídica)
+-- Tipo de persona, tipo de doc cliente, num doc cliente, nombres y apellidos cliente o razón social, monto acumulado
+
+
+
+
 
 
 -- Ingreso acumulados obtenidos por cada tipo de habitación
-select 
